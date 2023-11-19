5f62350954e7d59cbe50ffaf0dd21776030c3d12
Viet281101 <nva281101@gmail.com>
2023-11-19 05:00:15 +0100

    update

--- js/lvl_1/display-01.js
+++ js/lvl_1/display-01.js
@@ -8,12 +8,25 @@ class Display {
 
 		this.renderColor = function (color) {
 
-		this.buffer.fillStyle = color;
-		this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
-
+			this.buffer.fillStyle = color;
+			this.buffer.fillRect(
+				0, 0, 
+				this.buffer.canvas.width, 
+				this.buffer.canvas.height
+			);
 		};
 
-		this.render = function () { this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height); };
+		this.render = function () { 
+			this.context.drawImage(
+				this.buffer.canvas, 
+				0, 0, 
+				this.buffer.canvas.width, 
+				this.buffer.canvas.height, 
+				0, 0, 
+				this.context.canvas.width, 
+				this.context.canvas.height
+			); 
+		};
 
 		this.resize = function (event) {
 			var height, width;
@@ -21,8 +34,8 @@ class Display {
 			height = document.documentElement.clientHeight;
 			width = document.documentElement.clientWidth;
 
-			this.context.canvas.height = height - 32;
-			this.context.canvas.width = width - 32;
+			this.context.canvas.height = height;
+			this.context.canvas.width = width;
 
 			this.render();
 		};
--- js/lvl_1/main-01.js
+++ js/lvl_1/main-01.js
@@ -1,43 +1,39 @@
 
 window.addEventListener("load", function(event) {
 
-  "use strict";
+	"use strict";
 
-      ///////////////////
-    //// FUNCTIONS ////
-  ///////////////////
+	///////////////////
+	//// FUNCTIONS ////
+	///////////////////
 
-  var render = function() {
+	var render = function() {
+		display.renderColor(game.color);
+		display.render();
+	};
 
-    display.renderColor(game.color);
-    display.render();
+	var update = function() {
+		game.update();
+	};
 
-  };
+		/////////////////
+		//// OBJECTS ////
+		//////////////////
+		
+		var controller = new Controller();
+		var display    = new Display(document.querySelector("canvas"));
+		var game       = new Game();
+		var engine     = new Engine(1000/30, render, update);
 
-  var update = function() {
+		////////////////////
+		//// INITIALIZE ////
+		////////////////////
 
-    game.update();
+		window.addEventListener("resize",  display.handleResize);
+		window.addEventListener("keydown", controller.handleKeyDownUp);
+		window.addEventListener("keyup",   controller.handleKeyDownUp);
 
-  };
-
-        /////////////////
-      //// OBJECTS ////
-    //////////////////
-    
-    var controller = new Controller();
-    var display    = new Display(document.querySelector("canvas"));
-    var game       = new Game();
-    var engine     = new Engine(1000/30, render, update);
-
-        ////////////////////
-      //// INITIALIZE ////
-    ////////////////////
-
-    window.addEventListener("resize",  display.handleResize);
-    window.addEventListener("keydown", controller.handleKeyDownUp);
-    window.addEventListener("keyup",   controller.handleKeyDownUp);
-
-    display.resize();
-    engine.start();
+		display.resize();
+		engine.start();
 
 });
